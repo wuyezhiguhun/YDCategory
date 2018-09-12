b6d3e3b8a30915b4cf6a2ad864894267ed1ba5d9
wuyezhiguhun <446310852@qq.com>
2018-09-12 21:13:02 +0800

    添加各种提示。

--- YDCategory/UIViewController+YDCategory.h
+++ YDCategory/UIViewController+YDCategory.h
@@ -26,4 +26,35 @@
             acceptHandler:(void (^)(UIAlertAction *action))acceptHandler
             cancelHandler:(void (^)(UIAlertAction *action))cancelHandler;
 
+/**
+ * 两个按钮消息提示空间 带类型
+ * @pram title          标题
+ * @pram message        消息
+ * @pram accept         确定 按钮 名称
+ * @pram cancel         取消 按钮 名称
+ * @pram style          提示框类型
+ * @pram acceptHandler  确定 按钮 点击 back回调
+ * @pram cancelHandler  取消 按钮 点击 back回调
+ */
+- (void)messageAlertTitle:(NSString *)title
+                  message:(NSString *)message
+                   accept:(NSString *)accept
+                   cancel:(NSString *)cancel
+           preferredStyle:(UIAlertControllerStyle)style
+            acceptHandler:(void (^)(UIAlertAction *action))acceptHandler
+            cancelHandler:(void (^)(UIAlertAction *action))cancelHandler;
+
+/**
+ * 一个按钮消息提示空间 带类型
+ * @pram title          标题
+ * @pram message        消息
+ * @pram accept         确定 按钮 名称
+ * @pram style          提示框类型
+ * @pram acceptHandler  确定 按钮 点击 back回调
+ */
+- (void)messageAlertTitle:(NSString *)title
+                  message:(NSString *)message
+                   accept:(NSString *)accept
+           preferredStyle:(UIAlertControllerStyle)style
+            acceptHandler:(void (^)(UIAlertAction *action))acceptHandler;
 @end
